b57e8f9af8b02630424cd048cc3e2d2cd89560c6
Johannes Schwinger <joderpro28@gmail.com>
2020-03-06 14:33:57 +0100

    Bot is now responding to input

--- demos/first_bot.py
+++ demos/first_bot.py
@@ -1,6 +1,7 @@
 import logging
 
-from telegram.ext import Updater, CallbackContext, CommandHandler, Dispatcher
+from telegram import Update
+from telegram.ext import Updater, CallbackContext, CommandHandler, Dispatcher, MessageHandler, Filters
 
 updater = Updater(token='756295721:AAFPvKk8A0wSJ7RxBx2IzEsDHJ2BPc9GYS4', use_context=True)
 dispatcher = updater.dispatcher
@@ -10,12 +11,22 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 
-def start(update, context: CallbackContext):
+def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Gib bitte noch einmal /start ein um zu überprüfen ob eh alles passt")
 
 
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Message:[{}], Sender[{}]'.format(update.message.text,
+                                                                    update.effective_user.username))
+
+    print('Message:[{}], Sender[{}]'.format(update.message.text, update.effective_user.username))
+
+
+echo_handler = MessageHandler(Filters.text, echo)
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
