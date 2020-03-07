0edee7198cf94ccdcdc93b39962e8792526be5bc
Johannes Schwinger <joderpro28@gmail.com>
2020-03-07 12:05:17 +0100

    fixed path

--- config/load.py
+++ config/load.py
@@ -6,7 +6,7 @@ def load_token() -> str:
     """Loads the json file that contains the API token.
     :returns the API token"""
     cur_path = os.path.dirname(__file__)
-    new_path = os.path.relpath('..\\config\\api_token.json', cur_path)
+    new_path = cur_path + "\\api_token.json"
 
     file =open(new_path, "r", encoding="utf8")
     json_file = json.load(file)
