e83f1a689249be3e54921b9f2a6880b0225baecc
Johannes Schwinger <joderpro28@gmail.com>
2020-03-07 12:40:31 +0100

    make sure .json exists
    .json should contain the API token
    make sure .json contains "token"

--- config/load.py
+++ config/load.py
@@ -2,15 +2,20 @@ import json
 import os
 
 
-def load_token() -> str:
+def load_token():
     """Loads the json file that contains the API token.
     :returns the API token"""
     cur_path = os.path.dirname(__file__)
     new_path = cur_path + "\\api_token.json"
-
-    file =open(new_path, "r", encoding="utf8")
-    json_file = json.load(file)
-    return json_file["token"]
+    try:
+        file = open(new_path, "r", encoding="utf8")
+    except FileNotFoundError:
+        print('The "api_token.json" is missing. Put this file in the config folder before running the program.')
+        raise
+    try:
+        json_file = json.load(file)["token"]
+        return json_file
+    except KeyError as e:
+        raise KeyError("Missing token in {}!".format(new_path)) from e
 
 # print(load_token())
-# print(load_token()["token"])
