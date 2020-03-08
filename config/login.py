3d014ed75c53141d487167eb7b761ac453a44f6b
Johannes Schwinger <joderpro28@gmail.com>
2020-03-08 17:09:39 +0100

    login.py can request user_token now

--- config/login.py
+++ config/login.py
@@ -2,11 +2,13 @@ import json
 import os
 from getpass import getpass
 
+import spotify_token
 
-def load_file():
+
+def load_file(open_mode="r"):
     cur_path = os.path.dirname(__file__)
     path = cur_path + "\\login.json"
-    file = open(path, "w", encoding="utf8")
+    file = open(path, open_mode, encoding="utf8")
     return file
 
 
@@ -23,10 +25,19 @@ def save_in_json(username, password):
         "username": username,
         "password": password
     }
-    file = load_file()
+    file = load_file("w")
     json.dump(input_login, file)
+    file.close()
 
     # print(json_file)
+
+
+def get_user_token():
+    file = load_file()
+    credentials = json.loads(file)
+    token = spotify_token.start_session(credentials["username"],
+                                        credentials["password"])
+    return token[0]
 
 
 request_login_info()
