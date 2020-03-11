6134bdab9eae6ee8f5bc0b88478363fd6548a8bc
Johannes Schwinger <joderpro28@gmail.com>
2020-03-11 13:18:59 +0100

    login.py no longer saves login credentials

--- config/login.py
+++ config/login.py
@@ -1,8 +1,8 @@
 import json
 import os
-from getpass import getpass
 
 import spotify_token
+from requests import HTTPError
 
 
 def load_file(open_mode="r"):
@@ -12,33 +12,55 @@ def load_file(open_mode="r"):
     return file
 
 
-def request_login_info():
+def request_login_info() -> dict:
     print("[Login to your Spotify account!]")
     print("Note, that your account needs to be Premium to work properly.")
     username = input("Username/e-mail: ")
-    password = getpass("Password: ")
-    save_in_json(username, password)
+    # password = getpass("Password: ")
+    password = input("Password: ")
+    return {'username': username, 'password': password}  # this is a dict()
 
 
-def save_in_json(username, password):
-    input_login = {
-        "username": username,
-        "password": password
+def save_in_json(token: list):
+    input_token = {
+        "token": token[0],
+        "expiration_date": token[1]
     }
     file = load_file("w")
-    json.dump(input_login, file)
+    json.dump(input_token, file)
     file.close()
 
     # print(json_file)
 
 
-def get_user_token():
-    file = load_file()
-    credentials = json.loads(file)
+def request_user_token(credentials: dict) -> list:
     token = spotify_token.start_session(credentials["username"],
                                         credentials["password"])
-    return token[0]
+    return token
 
 
-request_login_info()
-input()
+def get_user_token() -> str:
+    file = load_file()
+    try:
+        json_file = json.load(file)
+    except KeyError as e:
+        raise KeyError("You are not logged into your Spotify account!") from e
+    token = json_file["token"]
+    file.close()
+    return token
+
+
+def load_new_user_token():
+    while True:
+        login_credentials = request_login_info()
+        try:
+            token = request_user_token(login_credentials)
+            save_in_json(token)
+            break
+        except HTTPError:
+            print("Login failed! Please reenter Login-Credentials.\n\n")
+            continue
+
+
+# print(get_user_token())
+load_new_user_token()
